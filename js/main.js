73059a7868079c7949d058abbb6e6a3ff179b049
Depressia <cray86@mail.ru>
2020-06-11 02:56:09 +0300

    Создаёт и заполняет карточки с описанием жилья.

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var ROOMS = {min: 1, max: 5};
 var GUESTS = {min: 1, max: 10};
 var CHECKIN_LIST = ['12:00', '13:00', '14:00'];
 var CHECKOUT_LIST = ['12:00', '13:00', '14:00'];
-var FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var FEATURES_LIST = [' wifi', ' dishwasher', ' parking', ' washer', ' elevator', ' conditioner'];
 var DESCRIPTION = 'Любой каприз за ваши шекели!';
 var PHOTOS_LIST = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
@@ -107,13 +107,14 @@ for (i = 0; i < TOTAL_AMOUNT; i++) {
   randomOffer[i] = getRandomHouse();
 }
 
-//  Отрисовываем метку на карте с автаром через scr и зоголовов в атрибуте alt
+// Находим темплейт для маркера.
 var pinElement = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-var renederPin = function (data) {
+// Функция для заполнения маркера.
+var renderPin = function (data) {
   var offerElement = pinTemplate.cloneNode(true);
 
   offerElement.querySelector('img').src = data.author.avatar;
@@ -125,8 +126,9 @@ var renederPin = function (data) {
 
 var fragment = document.createDocumentFragment();
 
+//  Отрисовываем маркер на карте.
 for (i = 0; i < randomOffer.length; i++) {
-  fragment.appendChild(renederPin(randomOffer[i]));
+  fragment.appendChild(renderPin(randomOffer[i]));
 }
 
 pinElement.appendChild(fragment);
@@ -134,3 +136,64 @@ pinElement.appendChild(fragment);
 //  Находим блок map и показываем его.
 var siteMap = document.querySelector('.map');
 siteMap.classList.remove('map--faded');
+
+// Находим темплейт для карточки с описанием.
+var cardElement = document.querySelector('.map__filters-container');
+var cardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.map__card');
+
+// Функция для проверки типа жилья.
+var getCheckType = function (type) {
+  if (type === 'Palace') {
+    return 'Дворец';
+  }
+  if (type === 'Flat') {
+    return 'Квартира'
+  }
+  if (type === 'House') {
+    return 'Дом'
+  }
+  if (type === 'Bungalo') {
+    return 'Бунгало'
+  }
+};
+
+// Функция для заполнения карточки с описанием.
+var renderCard = function (data) {
+  var mapCardElement = cardTemplate.cloneNode(true);
+  var photosLength = data.offer.photos.length;
+  var photoBlock = mapCardElement.querySelector('.popup__photos');
+  var photoElement = mapCardElement.querySelector('.popup__photos')
+    .querySelector('.popup__photo');
+
+  mapCardElement.querySelector('.popup__title').textContent = data.offer.title;
+  mapCardElement.querySelector('.popup__text--address').textContent = data.offer.address;
+  mapCardElement.querySelector('.popup__text--price').textContent = data.offer.price + '₽/ночь';
+  mapCardElement.querySelector('.popup__type').textContent = getCheckType(data.offer.type);
+  mapCardElement.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests;
+  mapCardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+  mapCardElement.querySelector('.popup__features').textContent = data.offer.features;
+  mapCardElement.querySelector('.popup__description').textContent = data.offer.description;
+  mapCardElement.querySelector('.popup__avatar').src = data.author.avatar;
+
+  photoElement.src = data.offer.photos[0];
+  if (photosLength > 1) {
+    for (var j = 1; j < photosLength; j++) {
+      var photoElementTemplate = photoElement.cloneNode(true);
+      photoElementTemplate.src = data.offer.photos[j];
+      photoBlock.appendChild(photoElementTemplate);
+    }
+  }
+
+  return mapCardElement;
+};
+
+var fragmentCard = document.createDocumentFragment();
+
+//  Отрисовываем карточку на карте.
+for (i = 0; i < randomOffer.length; i++) {
+  fragmentCard.appendChild(renderCard(randomOffer[i]));
+}
+
+siteMap.insertBefore(fragmentCard, cardElement);
