9211e6d181494c8f807a402c4ced290000aec46f
Depressia <cray86@mail.ru>
2020-06-25 18:58:32 +0300

    Выносит код для обработки действий с меткой в pin.js

--- js/pin.js
+++ js/pin.js
@@ -15,8 +15,38 @@
     return offerElement;
   };
 
+  var inputAddress = document.querySelector('#address');
+  var mapPinMain = document.querySelector('.map__pin--main');
+  var mapPinMainWidth = 65;
+  var mapPinMainHeigth = 84;
+  var mapPinMainPositionX = Math.floor(570 + mapPinMainWidth / 2);
+  var mapPinMainPositionY = Math.floor(375 + mapPinMainHeigth / 2);
+
+  var newPosition = function () {
+    mapPinMainPositionX = Math.floor(parseInt(mapPinMain.style.left, 10) + mapPinMainWidth / 2);
+    mapPinMainPositionY = Math.floor(parseInt(mapPinMain.style.top, 10) + mapPinMainHeigth);
+    inputAddress.value = mapPinMainPositionX + ', ' + mapPinMainPositionY;
+  };
+
+  inputAddress.value = mapPinMainPositionX + ', ' + mapPinMainPositionY;
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    if (evt.button === 0) {
+      window.map.enableSite();
+      window.map.startMap();
+    }
+  });
+
+  mapPinMain.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      window.map.enableSite();
+      window.map.startMap();
+    }
+  });
+
   window.pin = {
-    renderPin: renderPin
+    renderPin: renderPin,
+    newPosition: newPosition
   };
 
 })();
